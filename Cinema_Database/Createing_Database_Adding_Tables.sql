a13bdf9f6c6df37a231d9ee0f51c8afca5f74e0b
sewerynstawiarski <seweryn.stawiarski@gmail.com>
2024-04-22 18:11:47 +0200

    Creating Cinema_bookings_system database and adding data

---
+++ Cinema_Database/Createing_Database_Adding_Tables.sql
@@ -0,0 +1,70 @@
+SHOW DATABASES;
+
+CREATE DATABASE cinema_booking_system;
+
+USE cinema_booking_system;
+
+CREATE TABLE films(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name varchar(45) NOT NULL UNIQUE,
+length_min INT NOT NULL
+);
+
+SHOW TABLES;
+
+SELECT * FROM films;
+
+DESCRIBE films;
+
+CREATE TABLE customers(
+id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(45),
+last_name VARCHAR(45) NOT NULL,
+email VARCHAR(45) NOT NULL UNIQUE
+);
+
+SHOW TABLES;
+DESCRIBE customers;
+
+CREATE TABLE rooms(
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(45) NOT NULL,
+no_seats INT NOT NULL
+);
+
+SHOW TABLES;
+DESCRIBE rooms;
+
+CREATE TABLE screenings(
+id INT PRIMARY KEY AUTO_INCREMENT,
+film_id INT NOT NULL,
+room_id INT NOT NULL,
+start_time DATETIME NOT NULL,
+FOREIGN KEY (film_id) REFERENCES films(id),
+FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+SHOW TABLES;
+DESCRIBE screenings;
+
+CREATE TABLE seats (
+id INT PRIMARY KEY AUTO_INCREMENT,
+seat_row CHAR(1) NOT NULL,
+number INT NOT NULL,
+room_id INT NOT NULL,
+FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+SHOW TABLES;
+DESCRIBE seats;
+
+CREATE TABLE bookings(
+id INT PRIMARY KEY AUTO_INCREMENT,
+screening_id INT NOT NULL,
+customer_id INT NOT NULL,
+FOREIGN KEY (screening_id) REFERENCES screenings(id),
+FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+SHOW TABLES;
+DESCRIBE bookings;
